95800f8bd811d5a8d8b360bc57cd64fcea74daf1
Jyotsna <jagjyotsna@gmail.com>
2016-06-24 10:38:57 +0530

    changed the order of forms declaration

--- ta/forms.py
+++ ta/forms.py
@@ -1,5 +1,20 @@
 from django import forms
 
+
+class ContingentForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(ContingentForm, self).__init__(*args, **kwargs)
+
+        for field in self.fields.itervalues():
+            field.widget.attrs['class'] = 'form-control'
+
+    emp_id = forms.IntegerField(label='Your employee id')
+    reached_date = forms.DateField(label='Reached date')
+    return_date = forms.DateField(label='Return date')
+    rate = forms.IntegerField(label='Rate of travel')
+    kms = forms.IntegerField(label='Average kms travelled daily')
+
+
 class NameForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(NameForm, self).__init__(*args, **kwargs)
@@ -14,17 +29,3 @@ class NameForm(forms.Form):
     return_train_no = forms.CharField(label='Return train no', max_length=5)
     f_date = forms.DateField(label='Journey finish date')
     f_time = forms.TimeField(label='Journey finish time')
-
-
-class ContingentForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super(ContingentForm, self).__init__(*args, **kwargs)
-
-        for field in self.fields.itervalues():
-            field.widget.attrs['class'] = 'form-control'
-
-	emp_id = forms.IntegerField(label='Your employee id')
-	reached_date = forms.DateField(label='Reached date')
-	return_date = forms.DateField(label='Return date')
-	rate = forms.IntegerField(label='Rate of travel')
-	kms = forms.IntegerField(label='Average kms travelled daily')
