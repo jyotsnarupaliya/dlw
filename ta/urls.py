4808353e0efdb7f09c1450c79781cb6a8bfea628
Ashutosh Dwivedi <adwivedi@akamai.com>
2016-06-26 11:45:49 +0530

    fixed sytax error in url.py

--- ta/urls.py
+++ ta/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^ta_form$', views.ta_form, name='ta_form'),
-    url(r'^contingent_form$', views.contingent_form, name='contingent_form')
-    url(r'^contingent_input_page$', views.contingent_input_page, name='contingent_input_page')
+    url(r'^contingent_form$', views.contingent_form, name='contingent_form'),
+    #url(r'^contingent_input_page$', views.contingent_input_page, name='contingent_input_page')
 
 ]
